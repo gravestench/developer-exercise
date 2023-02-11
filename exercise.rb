a88979e5b3f98a31d55194ce6053eee079ae4830
dylan knuth <dknuth0101@gmail.com>
2023-02-11 02:48:08 -0800

    implement fibonnacci method

--- exercise.rb
+++ exercise.rb
@@ -19,7 +19,38 @@ class Exercise
   # eg. the Fibonacci sequence up to 6 terms is (1, 1, 2, 3, 5, 8),
   # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
-    # TODO: Implement this method
+    sum = 0
+
+    while nth > 0
+      fibn = self.fib(nth) 
+      sum = sum + (fibn % 2 == 0 ? fibn : 0)
+      nth -= 1
+    end
+
+    return sum
   end
 
+  def self.fib(terms = 0)
+    if terms < 1
+      return 0
+    end
+
+    if terms == 1
+      return 1
+    end
+
+    count = 0
+    sum = 0
+    a = 0
+    b = 1
+    
+    while count < terms
+      sum = a+b
+      a = b
+      b = sum
+      count += 1
+    end
+
+    return a
+  end
 end
